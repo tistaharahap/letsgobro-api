aa3bb677b340ab83224340fa00c3a589e528b732
Batista Harahap <batista@bango29.com>
2021-05-16 23:52:31 +0700

    add docker

--- app.py
+++ app.py
@@ -12,6 +12,6 @@ app.include_router(Airports, prefix='/v1', tags=['v1'])
 if __name__ == '__main__':
     uvicorn.run('app:app',
                 host='0.0.0.0',
-                port=8000,
+                port=8080,
                 debug=True,
                 log_level='debug')
